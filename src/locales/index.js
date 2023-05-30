e6061c6332e05eb045001438ef3c06ac6468b7fa
waynekim <eunchan0817@gmail.com>
2023-05-31 01:25:07 +0900

    feat: support korea

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,9 +1,11 @@
 import deDe from './de-de.js'
 import enUs from './en-us.js'
 import zhCn from './zh-cn.js'
+import koKr from './ko-kr.js'
 
 export default {
   'de-de': deDe,
   'en-us': enUs,
-  'zh-cn': zhCn
+  'zh-cn': zhCn,
+  'ko-kr': koKr
 }
