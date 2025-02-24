b28ba3b4a3cbb8addba3c9d573a3fbee28481779
Jerry Bruwes <jbruwes@icloud.com>
2025-02-24 13:42:24 +0200

    modified:   src/locales/index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,11 +3,13 @@ import enUs from './en-us.js'
 import zhCn from './zh-cn.js'
 import koKr from './ko-kr.js'
 import jaJp from './ja-jp.js'
+import ruRu from './ru-ru.js'
 
 export default {
   'de-de': deDe,
   'en-us': enUs,
   'zh-cn': zhCn,
   'ko-kr': koKr,
-  'ja-jp': jaJp
+  'ja-jp': jaJp,
+  'ru-ru': ruRu
 }
